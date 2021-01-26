8afd8f298f07b6a50529f02a5a15bbe1aa647604
Max Rand <dopler168@gmail.com>
2021-01-26 20:35:58 +0300

    add crowdsale tests

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,14 +1,7 @@
 const Token = artifacts.require("MyToken");
 
-var chai = require("chai");
-
+const chai = require("./setupchai.js");
 const BN = web3.utils.BN;
-const chaiBN = require("chai-bn")(BN);
-chai.use(chaiBN);
-
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-
 const expect = chai.expect;
 
 require('dotenv').config({path: '../.env'});
@@ -23,7 +16,7 @@ contract("Token Test", async accounts => {
     it("All tokens should be in my account", async () => {
         let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
-        expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+        return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
 
     it("I can send tokens from Account 1 to Account 2", async () => {
@@ -33,7 +26,7 @@ contract("Token Test", async accounts => {
         expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
         expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
         expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
-        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+        return expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
     });
 
     it("It's not possible to send more tokens than account 1 has", async () => { 
@@ -41,6 +34,6 @@ contract("Token Test", async accounts => {
         let balanceOfAccount = await instance.balanceOf(initialHolder);
         expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
         //check if the balance is still the same
-        expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
+        return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
     });
 });
