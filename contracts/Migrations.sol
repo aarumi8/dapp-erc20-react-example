fcd07ab31c80ad4d9fa518ea1752d5c3ec567b04
Max Rand <dopler168@gmail.com>
2021-01-26 17:34:26 +0300

    added package.json

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.7.0;
 
 contract Migrations {
-  
+
   address public owner;
   uint public last_completed_migration;
 
@@ -10,7 +10,7 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  constructor() public {
+  constructor() {
     owner = msg.sender;
   }
 
